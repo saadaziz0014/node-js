e7767a5c625f79e92af120951167086b4405f7f3
saadaziz <saadaziz0014@gmail.com>
2022-10-05 17:11:30 +0500

    simple server express

--- index.js
+++ index.js
@@ -1,54 +1,19 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
+const http = require9("http");
 
 const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(`Request for ${req.url} and method is ${req.method}`);
-  if (req.method == "GET") {
-    let fileURL;
-    if (req.url == "/") fileURL = "/index.html";
-    else fileURL = req.url;
+const app = express();
 
-    let filePath = path.resolve("./public" + fileURL);
-    const fileExt = path.extname(filePath);
-    if (fileExt == ".html") {
-      fs.exists(filePath, (exist) => {
-        if (!exist) {
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html");
-          res.end(
-            "<html><body><h1>Error 404: " +
-              fileUrl +
-              " not found</h1></body></html>"
-          );
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        fs.createReadStream(filePath).pipe(res);
-      });
-    } else {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      res.end(
-        "<html><body><h1>Error 404: " +
-          fileUrl +
-          " not a HTML file</h1></body></html>"
-      );
-    }
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      "<html><body><h1>Error 404: " +
-        req.method +
-        " not supported</h1></body></html>"
-    );
-  }
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
