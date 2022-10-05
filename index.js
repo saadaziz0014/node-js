21f1e997db054054422c3cc70fc515ef1c74282a
saadaziz <saadaziz0014@gmail.com>
2022-10-05 13:17:30 +0500

    using constraints with object

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const rect = {
 }
 
 function solver(l,b){
-    console.log(`Area for length ${l} and breadth ${b} is : ${rect.area(l,b)} and Perimeter is : ${rect.perimeter(l,b)} `);
+    if(l <= 0 || b <= 0){
+        console.log("Value for both l and b must be geater than zero");
+    }
+    else{
+        console.log(`Area for length ${l} and breadth ${b} is : ${rect.area(l,b)} and Perimeter is : ${rect.perimeter(l,b)} `);
+    }
 }
 
 solver(5,1);
+solver(0,2);
+solver(3,-8);
 
 
