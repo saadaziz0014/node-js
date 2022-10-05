15c12128feaf4214234a449b7b85361e115780e7
saadaziz <saadaziz0014@gmail.com>
2022-10-05 17:05:58 +0500

    http static file server

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req,res) =>{
-    console.log(req.headers);
+const server = http.createServer((req, res) => {
+  console.log(`Request for ${req.url} and method is ${req.method}`);
+  if (req.method == "GET") {
+    let fileURL;
+    if (req.url == "/") fileURL = "/index.html";
+    else fileURL = req.url;
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end("<html><body><h1>Saad with HTTP</h1></body></html>");
-})
+    let filePath = path.resolve("./public" + fileURL);
+    const fileExt = path.extname(filePath);
+    if (fileExt == ".html") {
+      fs.exists(filePath, (exist) => {
+        if (!exist) {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.end(
+            "<html><body><h1>Error 404: " +
+              fileUrl +
+              " not found</h1></body></html>"
+          );
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end(
+        "<html><body><h1>Error 404: " +
+          fileUrl +
+          " not a HTML file</h1></body></html>"
+      );
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      "<html><body><h1>Error 404: " +
+        req.method +
+        " not supported</h1></body></html>"
+    );
+  }
+});
 
-server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
-
-
-
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
