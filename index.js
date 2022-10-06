56a0428d9e1d2b6c0684fa7dd559a3510951e7ae
saadaziz <saadaziz0014@gmail.com>
2022-10-06 13:41:57 +0500

    simple REST in express

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const http = require("http");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
@@ -8,7 +9,53 @@ const port = 3000;
 const app = express();
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+
+app.all("/dishes", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+});
+
+app.get("/dishes", (req, res, next) => {
+  res.end("Will send all the dishes to you!");
+});
+
+app.post("/dishes", (req, res, next) => {
+  res.end(
+    `Will add the dish ${req.body.name} with details ${req.body.description} `
+  );
+});
+
+app.put("/dishes", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("PUT is not suported on /dishes");
+});
+
+app.delete("/dishes", (req, res, next) => {
+  res.end("Deleting all the dishes");
+});
+
+app.get("/dishes/:dishID", (req, res, next) => {
+  res.end(`Will send details of ${req.params.dishID} to you!`);
+});
+
+app.put("/dishes/:dishID", (req, res, next) => {
+  res.write(`Updating the details of ${req.params.dishID} \n`);
+  res.end(
+    `Will update the dish ${req.body.name} with details ${req.body.description}`
+  );
+});
+
+app.post("/dishes/:dishID", (req, res, next) => {
+  res.statusCode = 403;
+  res.end(`POST is not supported on /dishes/${req.params.dishID}`);
+});
+
+app.delete("/dishes/:dishID", (req, res, next) => {
+  res.end(`Deleting dish: ${req.params.dishID}`);
+});
 
 app.use((req, res, next) => {
   console.log(req.headers);
